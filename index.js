5b998aa6fa25032e54d8620c7ebb7d406a7d546e
Nazar <nazargritsik99@gmail.com>
2026-05-05 22:33:17 +0300

    Move database config to env

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const app = express();
 app.use(express.json());
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '1488',
-    database: 'bookstore'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 db.connect((err) => {
